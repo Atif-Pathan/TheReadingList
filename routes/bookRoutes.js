81b9857ec02167ea5282fb3abdf238a6f01d609f
Atif Pathan <atifkhan.p1@gmail.com>
2025-08-19 23:14:07 -0600

    Add routes for creating new books
    Added GET and POST routes for '/new' to handle displaying and submitting the create book form. This enables users to add new books through the application.

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -3,7 +3,10 @@ const { Router } = require('express');
 const bookController = require('../controllers/bookController');
 const bookRouter = Router();
 
+bookRouter.get('/new', bookController.createBookGet);
+bookRouter.post('/new', bookController.createBookPost);
 bookRouter.get('/:id', bookController.getBook);
+// bookRouter.get('/post', bookController.createBookPost);
 // bookRouter.get("/create", usersController.usersCreateGet);
 // bookRouter.post("/create", usersController.usersCreatePost);
 // bookRouter.get("/:id/update", usersController.usersUpdateGet);
